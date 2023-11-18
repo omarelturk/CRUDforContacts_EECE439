7dd4ff56b5e1663282412ef8910cc77d2df9dacf
omarelturk <86617257+omarelturk@users.noreply.github.com>
2023-11-18 19:19:53 +0200

    Update settings.py

--- CRUDforcontacts/CRUDforcontacts/settings.py
+++ CRUDforcontacts/CRUDforcontacts/settings.py
@@ -120,5 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Add these lines at the bottom of the file:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
